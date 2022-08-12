c11f9ae96f1f1e2663a3c8f69f5483dd570b8fc5
WillOP <wilfredp159@gmail.com>
2022-08-11 23:26:09 -0600

    Se agrego un endpoint 3000/sumar

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.get("/",(req,res) =>{
+    res.send("Hola mundo");
+})
+
+app.post("/sumar",(req,res) =>{
+    try {
+        var dato = req.body;
+        var num1 = dato.numero
+        var num2 = dato.numero2
+        var suma = num1 + num2
+        res.json({"resultado suma":suma});
+    }catch(err){
+        console.log("error: "+err)
+    }
+})
+
+app.listen(3000, ()=>{
+    console.log("Servidor corriendo en el puerto 3000");
+})
